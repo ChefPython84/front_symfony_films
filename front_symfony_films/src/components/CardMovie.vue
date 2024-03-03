5d4bb5f1e88c14f4401250ef9fed213908f1c89f
ChefPython84 <erwan.couturier@etudiant.univ-reims.fr>
2024-03-03 20:56:42 +0100

    feat(): ajout de cardmovie

---
+++ front_symfony_films/src/components/CardMovie.vue
@@ -0,0 +1,28 @@
+<script setup>
+import {defineProps} from 'vue';
+// eslint-disable-next-line no-unused-vars
+import MovieDetail from '@/views/MovieDetail.vue';
+
+defineProps({
+  movie: {
+    type: Object,
+    required: true
+  }
+})
+</script>
+
+<template>
+  <div class="card w-75">
+    <div class="card-body">
+      <h5 class="card-title">{{movie.title}}</h5>
+      <p class="card-text">{{movie.description}}</p>
+      <router-link :to="{ name: 'MovieDetail', params: { id : movie.id }}" class="btn btn-primary">En savoir plus</router-link>
+    </div>
+  </div>
+
+
+</template>
+
+<style scoped>
+
+</style>
