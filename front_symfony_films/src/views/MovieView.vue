8f46ed382cad8d25814d6d7543116970f7040f8f
ChefPython84 <erwan.couturier@etudiant.univ-reims.fr>
2024-03-03 23:13:57 +0100

    feat(MovieView.vue): ajout de la fonction de la pagination

--- front_symfony_films/src/views/MovieView.vue
+++ front_symfony_films/src/views/MovieView.vue
@@ -1,81 +1,144 @@
-<script setup>
-import {ref, onMounted} from "vue";
-import router from "../router";
-import CardMovie from '@/components/CardMovie.vue';
+<template>
+  <div class="container mt-5">
+    <h1>Films</h1>
+    <form class="d-flex">
+      <input
+        class="form-control me-2"
+        type="search"
+        placeholder="Search"
+        aria-label="Search"
+        v-model="searchInput"
+        @input="autoSearch"
+      />
+    </form>
+    <div class="my-5">
+      <div class="row my-5">
+        <div class="col">
+          <CardMovie
+            v-for="movie in paginatedMovies"
+            :key="movie.id"
+            :movie="movie"
+          />
+        </div>
+      </div>
+    </div>
+    <div>
+      <nav aria-label="Page navigation example">
+        <ul class="pagination">
+          <li class="page-item">
+            <a class="page-link" @click="changePage(-1)" :disabled="currentPage === 1" aria-label="Previous">
+              <span class="sr-only">Previous</span>
+            </a>
+          </li>
+          <li class="page-item"><a class="page-link">{{ currentPage }} / {{ totalPages }}</a></li>
+          <li class="page-item">
+            <a class="page-link" @click="changePage(1)" :disabled="currentPage === totalPages" aria-label="Next">
+              <span class="sr-only">Next</span>
+            </a>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  </div>
+</template>
 
-const movie = ref('');
-const searchInput = ref('');
+<script setup>
+import { ref, onMounted } from "vue";
+import router from "../router";
+import CardMovie from "@/components/CardMovie.vue";
+
+const movies = ref([]);
+const searchInput = ref("");
+const totalPages = ref(0);
+const currentPage = ref(1);
+const itemsPerPage = 10; // Nombre d'éléments par page
+const paginatedMovies = ref([]);
+
 const AuthenticationRequest = async () => {
-  const token = localStorage.getItem('token');
+  const token = localStorage.getItem("token");
   if (token) {
     try {
-      const response = await fetch("http://localhost:8000/api/movies", {
-        method: "GET",
-        headers: {
-          'Authorization': `Bearer ${token}`,
-        },
-      });
+      const response = await fetch(
+        `http://localhost:8000/api/movies`,
+        {
+          method: "GET",
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        }
+      );
       const jsonData = await response.json();
-      if (jsonData.code === 401){
+      if (jsonData.code === 401) {
         await router.push("/login");
         console.log("Vous n'êtes pas connecté");
       }
-      movie.value = jsonData['hydra:member'];
+      movies.value = jsonData["hydra:member"];
+      updatePagination();
     } catch (error) {
       console.log(error);
     }
-  }else {
+  } else {
     console.log("Vous n'êtes pas connecté");
   }
-}
-async function search() {
-  console.log(searchInput);
-  const token = localStorage.getItem('token');
+};
+
+const autoSearch = async () => {
+  if (searchInput.value.trim() !== "") {
+    await search();
+  } else {
+    await AuthenticationRequest();
+  }
+};
+
+const search = async () => {
+  const token = localStorage.getItem("token");
   if (token) {
     try {
-      const response = await fetch("http://localhost:8000/api/movies?title=" + searchInput.value, {
-        method: "GET",
-        headers: {
-          'Authorization': `Bearer ${token}`,
-        },
-      });
+      const response = await fetch(
+        `http://localhost:8000/api/movies?title=${searchInput.value}`,
+        {
+          method: "GET",
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        }
+      );
       const jsonData = await response.json();
-      if (jsonData.code === 401){
+      if (jsonData.code === 401) {
         await router.push("/login");
         console.log("Vous n'êtes pas connecté");
       }
-      movie.value = jsonData['hydra:member'];
+      movies.value = jsonData["hydra:member"];
+      updatePagination();
     } catch (error) {
       console.log(error);
     }
-  }else {
+  } else {
     console.log("Vous n'êtes pas connecté");
   }
-}
+};
 
 onMounted(async () => {
   await AuthenticationRequest();
 });
 
+const changePage = async (offset) => {
+  const newPage = currentPage.value + offset;
+  if (newPage >= 1 && newPage <= totalPages.value) {
+    currentPage.value = newPage;
+    updatePagination();
+  }
+};
+
+const updatePagination = () => {
+  const startIndex = (currentPage.value - 1) * itemsPerPage;
+  const endIndex = startIndex + itemsPerPage;
+  paginatedMovies.value = movies.value.slice(startIndex, endIndex);
+  totalPages.value = Math.ceil(movies.value.length / itemsPerPage);
+};
+
 </script>
 
-<template>
-  <div class="container mt-5">
-    <h1>Films</h1>
-    <form class="d-flex" @submit.prevent="search">
-      <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search" v-model="searchInput">
-      <button class="btn btn-outline-success" type="submit">Search</button>
-    </form>
-    <div class="my-5">
-      <div class="row my-5">
-        <div class="col">
-          <CardMovie v-for="movie in movie" :key="movie.id" :movie="movie"/>
-        </div>
-      </div>
-    </div>
-  </div>
-</template>
-
 <style scoped>
-
+/* Vos styles ici */
 </style>
